20b64eb005f85e7477cc7afbf8803c680aae5ff1
Brian Lagranda <brianlagranda@gmail.com>
2024-11-15 16:52:44 -0300

    feat: add Load More conditional rendering

--- src/components/Favourites.tsx
+++ src/components/Favourites.tsx
@@ -1,8 +1,8 @@
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 import { RootState } from '../app/store';
-
 import MediaGrid from './MediaGrid/MediaGrid';
 
 const Favourites = () => {
@@ -11,14 +11,30 @@ const Favourites = () => {
         (state: RootState) => state.favourite.favourites,
     );
 
+    const [visibleCount, setVisibleCount] = useState(
+        Math.min(favourites.length, 20),
+    );
+
+    useEffect(() => {
+        setVisibleCount(Math.min(favourites.length, 20));
+    }, [favourites]);
+
     if (!Token) return <Navigate to="/" />;
+
+    const handleLoadMore = () => {
+        setVisibleCount((prevCount) =>
+            Math.min(prevCount + 20, favourites.length),
+        );
+    };
+
+    const hasMoreItems = visibleCount < favourites.length;
 
     return (
         <MediaGrid
-            mediaList={favourites}
+            mediaList={favourites.slice(0, visibleCount)}
             loading={false}
             error={null}
-            onLoadMore={() => {}}
+            onLoadMore={hasMoreItems ? handleLoadMore : undefined}
         />
     );
 };
